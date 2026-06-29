7ec8cfd87f4ce316e2ddc248a05275e8ef85b042
Cheboy04 <cornejoadrian93@gmail.com>
2026-06-28 22:53:52 -0500

    Commit Ejercicio 2: Añadir un logotipo ASCII

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         Despedida despedida = new Despedida();
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
         System.out.println(despedida.generarDespedida("Mundo"));
         System.out.println(saludo.generarSaludo("Mundo"));
         System.out.println("Version 1.1");
